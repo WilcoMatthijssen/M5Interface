9f65308500cc1a8574aa84b49c1c30c6313393b2
Wilco <wilco@matthijssen.xyz>
2021-10-24 19:55:27 +0200

    Added example for weather layout

--- lib/navbar/navbar.h
+++ lib/navbar/navbar.h
@@ -1,45 +1,68 @@
+#pragma once
+
 #include <Arduino.h>
 #include <M5EPD.h>
 #include "secrets.h"
-class NavBar{
+
+
+#include <Free_Fonts.h>
+class M5Interface{
+    M5EPD_Canvas *_canvas;
 public:
+    M5Interface(M5EPD_Canvas  *canvas){ _canvas = canvas; }
+
     
-    void draw_date(const int& x, const int& y, M5EPD_Canvas& canvas){
+    void draw_date(const int& x, const int& y){
         rtc_date_t date;
         M5.RTC.getDate(&date);
 
-        canvas.drawString(String(date.day)+ "/" + String(date.mon) + "/" + String(date.year), x, y);
+        _canvas->drawString(String(date.day)+ "/" + String(date.mon) + "/" + String(date.year), x, y);
 
     }
 
-    void draw_battery(const int& x, const int& y, M5EPD_Canvas& canvas){
+    void draw_battery(const int& x, const int& y){
         float dirty_percentage = (float)(M5.getBatteryVoltage() - 3300) / (float)(4350 - 3300);
         const int percentage = constrain(dirty_percentage * 100, 0, 100);
 
-        canvas.drawString( String(percentage) + "%", x, y);
+        _canvas->drawString( String(percentage) + "%", x, y);
     }
 
 
+    void draw_weather_image(int x, int y, String weather){
+        // I should be checking if the file even exists but the drawPngFile function does it for me to some extent.
+        String filename = "/" + weather + ".png";
+        _canvas->drawPngFile(SD, filename.c_str(), x, y);
+    }
 
-    void draw(M5EPD_Canvas& canvas){
-        canvas.fillRect(0, 0, 540, 100, 15);
-        canvas.fillRect(5, 5, 530, 90, 0);
 
+    void draw(){
+        _canvas->fillRect(0, 0, 540, 60, _canvas->G15);
+        _canvas->fillRect(5, 5, 530, 50, _canvas->G0);
 
-        canvas.fillRect(0, 0, 540, 100, 15);
-        canvas.fillRect(0, 100, 540, 100, 13);
-        canvas.fillRect(0, 200, 540, 100, 11);
-        canvas.fillRect(0, 300, 540, 100, 9);
-        canvas.fillRect(0, 400, 540, 100, 7);
-        canvas.fillRect(0, 500, 540, 100, 5);
-        canvas.fillRect(0, 600, 540, 100, 3);
-        canvas.fillRect(0, 700, 540, 100, 1);
-        canvas.fillRect(0, 800, 540, 100, 0);
-    
-        
-    
+        String weather = "Weather";
+
+        _canvas->setTextSize(1);
+        _canvas->setFreeFont(FF20);
+
+        draw_weather_image(40,150,weather);
   
-        draw_battery(400, 10, canvas);
-        draw_date(100, 10, canvas);
+        draw_battery(400, 10);
+        draw_date(100, 10);
+
+
+
+        _canvas->setFreeFont(FF32);
+
+        _canvas->drawString("Neerslagkans: 40%", 10, 600);
+        _canvas->drawString("Temperatuur: 17.5C\n", 10, 650);
+
+        _canvas->setFreeFont(FF19);
+        _canvas->setCursor(0, 700);
+        _canvas->println("Zonnig, morgen overwegend bewolkt en af en toe regen");
+        _canvas->setTextSize(4);
+        // _canvas->drawString("Hello World", 45, 105);
+        // _canvas->drawString("Neerslagkans: 40%", 45, 700);
+        // _canvas->drawString("Temperatuur: 17.5C", 345, 700);
+        // _canvas->drawString("Zonnig, morgen overwegend bewolkt en af en toe regen", 45, 750);
     }
 };
--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,13 @@
 #include <WiFi.h>
 #include "navbar.h"
 #include "secrets.h"
+#include <HTTPClient.h>
+#include <ArduinoJson.h>
 
 M5EPD_Canvas canvas(&M5.EPD);
-
-NavBar nv;
+	
+HTTPClient http;
+M5Interface nv(&canvas);
 
 void setup(){
     M5.begin();
@@ -18,19 +21,48 @@ void setup(){
     canvas.createCanvas(540, 960);
 
     canvas.setTextSize(4);
+
+    // WiFi.begin(SSID, PASSWORD);
+    // while (WiFi.status() != WL_CONNECTED) {
+    // delay(500);
+    //     Serial.print(".");
+    // }
+    // Serial.print("WiFi connected with IP: ");
+    // Serial.println(WiFi.localIP());
+
+    // http.useHTTP10(true);
+    // String request = "https://weerlive.nl/api/json-data-10min.php?key=" + String( WEATHER_API_KEY ) + "&locatie="+ String(LOCATION);
+    // http.begin(request);
+    // http.GET();
+
+    // DynamicJsonDocument doc(2048);
+    // deserializeJson(doc, http.getStream());
+
+    
+    // // Disconnect
+    // http.end();
+    // String output;
+    // serializeJson(doc, output);
+    // Serial.println(output);
     
 }
 // #include <Free_Fonts.h>
 void loop(){
-     
+
     canvas.fillCanvas(0);
 
-    // canvas.setFreeFont(FM9);
+  
+    // canvas.drawPngUrl("http://upload.wikimedia.org/wikipedia/commons/thumb/f/f0/Weather_Icons_-_bkn.svg/240px-Weather_Icons_-_bkn.svg.png", 300 ,300);
 
-    canvas.drawString("Hello World", 45, 350);
-    nv.draw(canvas);
+    // canvas.drawPngFile(SD, "/Weather.png", 300, 300);
+
+
+    
+    nv.draw();
 
     
     canvas.pushCanvas(0,0,UPDATE_MODE_GC16);
+    
     delay(10000);
+
 }
