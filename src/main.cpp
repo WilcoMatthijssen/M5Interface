27a1a4b1bb92639d2a3062f046bae88c483c026b
Wilco <wilco@matthijssen.xyz>
2021-11-03 14:38:51 +0100

    Minimized time nothing is displayed when updated

--- src/main.cpp
+++ src/main.cpp
@@ -9,18 +9,16 @@
 #include <ArduinoJson.h>
 
 M5EPD_Canvas canvas(&M5.EPD);
-	
+
 HTTPClient http;
-M5Interface nv(&canvas);
+M5Interface weatherdisplay(&canvas);
+
+
+
 
 void setup(){
     M5.begin();
-    M5.EPD.SetRotation(90);
-    M5.EPD.Clear(true);
-    M5.RTC.begin();
-    canvas.createCanvas(540, 960);
-
-    canvas.setTextSize(4);
+    
 
     WiFi.begin(SSID, PASSWORD);
     while (WiFi.status() != WL_CONNECTED) {
@@ -31,32 +29,39 @@ void setup(){
     Serial.println(WiFi.localIP());
 
     http.useHTTP10(true);
-    String request = "http://weerlive.nl/api/json-data-10min.php?key=" + String( WEATHER_API_KEY ) + "&locatie="+ String(LOCATION);
+    String request = "http://weerlive.nl/api/json-data-10min.php?key=" + String( WEATHER_API_KEY ) + "&locatie="+ String( LOCATION );
     http.begin(request);
     http.GET();
 
     DynamicJsonDocument doc(2048);
     deserializeJson(doc, http.getStream());
     http.end();
- 
-
-    JsonObject obj = doc["liveweer"][0].as<JsonObject>();
+    JsonObject weather_info = doc["liveweer"][0].as<JsonObject>();
     // serializeJson(obj, Serial);
 
 
-    canvas.fillCanvas(0);
-    nv.draw(obj);
+    M5.EPD.SetRotation(90);
+    M5.RTC.begin();
+    canvas.createCanvas(540, 960);
+    weatherdisplay.draw(weather_info);
+
+    // Removes shadow stripes in display
+    M5.EPD.Clear(true);
+
+    // Update display with highest quality
     canvas.pushCanvas(0,0,UPDATE_MODE_GC16);
     delay(500);
-    
-    RTC_Time sleep_time;
-    M5.RTC.getTime(&sleep_time);
-    sleep_time.hour += 1;
-    sleep_time.min   = 0;
-    sleep_time.sec   = 0;
 
-    M5.shutdown(sleep_time);
 
 }
 
-void loop(){}
+void loop(){
+    // sleep for 1 hour
+    // inside loop to prevent skipping sleep when powered from usb.
+    RTC_Time sleep_time;
+    M5.RTC.getTime(&sleep_time);
+    sleep_time.hour  = (sleep_time.hour + 1) % 24;
+    sleep_time.min   = 0;
+    sleep_time.sec   = 0;
+    M5.shutdown(sleep_time);
+    }
